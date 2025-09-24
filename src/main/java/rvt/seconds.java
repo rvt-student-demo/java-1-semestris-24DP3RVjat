d5d67eb9b17ba52178c601c5e16d2e3593d59398
24DP3RVjat <a240371rv@rvt.lv>
2025-09-24 14:52:08 +0300

    Izdariju jaunus uzdevumus

---
+++ src/main/java/rvt/seconds.java
@@ -0,0 +1,88 @@
+package rvt;
+
+public class seconds {
+    public static void main(String[] args) {
+        int calculationWithParentheses = (1 + 1) + 3 * (2 + 5);
+        System.out.println(calculationWithParentheses); // printe kko
+        int calculationWithoutParentheses = 1 + 1 + 3 * 2 + 5;
+        System.out.println(calculationWithoutParentheses); // izprinte skaitli
+
+        int interesanti_skaitli = (69 + 67) * (2 * 5);
+        System.out.println(interesanti_skaitli);
+        int a = 67;
+        int b = 69;
+        int c = 124;
+        int skibidi_fw = a + b + c;
+        System.out.println(skibidi_fw);
+
+        int valdis = 17;
+        int janis = 15;
+        int petrs = 16; 
+        int bernu_skaits = valdis + janis + petrs;
+        System.out.println(bernu_skaits);
+
+        int daunis = 5;
+        int gudrais = 10;
+        int kopaa = daunis + gudrais;
+        System.out.println(kopaa);
+
+        int flugina = 100;
+        int flugina2 = 200;
+        int flugina3 = 300;
+        int flugina_kopaa = flugina + flugina2 + flugina3;
+        System.out.println(flugina_kopaa);
+
+        int x = 10;
+        int y = 20;
+        int z = 30;
+        int sum = x + y + z;
+        System.out.println(sum);
+
+        int alpha = 7;
+        int beta = 14;
+        int gamma = 21;
+        int total = alpha + beta + gamma;
+        System.out.println(total);
+
+        int pirmais = 3;
+        int otrais = 6;
+        int tresais = 9;
+        int summa = pirmais + otrais + tresais;
+        System.out.println(summa);
+
+        int viens = 4;
+        int divi = 8;
+        int tris = 12;
+        int kopums = viens + divi + tris;
+        System.out.println(kopums);
+
+        int sk1 = 11;
+        int sk2 = 22;
+        int sk3 = 33;
+        int rezultats = sk1 + sk2 + sk3;
+        System.out.println(rezultats);
+
+        System.out.println(
+            "Perimetrs: " + (5 + 5 + 5 + 5)
+        );
+        
+        int number = 35;
+        if (number > 30) {
+            System.out.println("sk lielaks pa 30");
+        } else if (number > 20) {
+            System.out.println("sk lielaks pa 20");
+        } else if (number > 10) {
+        System.out.println("sk lielaks pa 10");
+
+        int Citsskaitlis = 55;
+        if (Citsskaitlis != 0) {
+        System.out.println("The number is not equal to 0");
+        if (Citsskaitlis >= 1000) {
+        System.out.println("The number is at least 1000");
+        }
+    }
+
+}
+
+    }
+}
