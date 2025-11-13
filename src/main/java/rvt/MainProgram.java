1c26e97562105caf0d72ef825e179ce9ee152d22
24DP3RVjat <a240371rv@rvt.lv>
2025-11-13 14:41:58 +0200

    Sveicināti! Komitoju, šodienas noslēdzošo darbu, par - Balansu un kartēm un to izmaiņām. Paldies. :)

---
+++ src/main/java/rvt/MainProgram.java
@@ -0,0 +1,58 @@
+package rvt;
+
+public class MainProgram {
+    public static void main(String[] args) {
+
+        PaymentCard paulsCard = new PaymentCard(20);
+        PaymentCard mattsCard = new PaymentCard(30);
+
+        paulsCard.eatHeartily();
+        mattsCard.eatAffordably();
+        System.out.println("Paul: " + paulsCard);
+        System.out.println("Matt: " + mattsCard);
+
+        paulsCard.addMoney(20);
+        mattsCard.eatHeartily();
+        System.out.println("Paul: " + paulsCard);
+        System.out.println("Matt: " + mattsCard);
+
+        paulsCard.eatAffordably();
+        paulsCard.eatAffordably();
+        mattsCard.addMoney(50);
+        System.out.println("Paul: " + paulsCard);
+        System.out.println("Matt: " + mattsCard);
+    }
+}
+
+class PaymentCard {
+    private double balance;
+
+    public PaymentCard(double openingBalance) {
+        this.balance = openingBalance;
+    }
+
+    public void eatAffordably() {
+        if (this.balance >= 2.60) {
+            this.balance -= 2.60;
+        }
+    }
+
+    public void eatHeartily() {
+        if (this.balance >= 4.60) {
+            this.balance -= 4.60;
+        }
+    }
+
+    public void addMoney(double amount) {
+        if (amount > 0) {
+            this.balance += amount;
+            if (this.balance > 150) {
+                this.balance = 150;
+            }
+        }
+    }
+
+    public String toString() {
+        return "The card has a balance of " + this.balance + " euros";
+    }
+}
